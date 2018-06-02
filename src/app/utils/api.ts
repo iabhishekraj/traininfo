f626bc3a9f9c5970f6d9edf71d0254cc04417436
Abhishek Raj <abhishek.raj@nagarro.com>
2018-06-02 17:50:48 +0530

    first push

--- src/app/utils/api.ts
+++
@@ -1,2 +0,0 @@
-//new api key - 5pf163qdia
-export const API = "d9pk6io6im";
--- src/app/utils/changeDateFormat.ts
+++
@@ -1,5 +0,0 @@
-export function changeFormat(date){
-    let dates = date.split('-');
-    let formatedDate = `${dates[2]}-${dates[1]}-${dates[0]}`;
-    return formatedDate;
-}
